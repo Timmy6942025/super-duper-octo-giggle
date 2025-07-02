6cc4f1e46d62d8e1a6855d4f45797b44c2f68409
Timmy6942025 <timothy.thomas2011@hotmail.com>
2025-07-02 20:31:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ vectorstore = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deseri
 
 llm = GeminiLLM(
     api_key="AIzaSyC__4SYJyttUSRsszBXAscUULwfDbiv84M",
-    model="gemini-2.5-flash"
-    ",
+    model="gemini-2.5-flash",
     temperature=0.0
 )
 qa_chain = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(search_kwargs={"k": 4}))
